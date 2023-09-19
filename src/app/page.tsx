a316d667bc9c6c485c79d8689bb5c1ede9656ff5
Alexis-Contente <alexis.contente@gmail.com>
2023-09-19 15:25:17 +0200

    base footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import Footer from "@/components/footer/footer";
 import Header from "@/components/header/header";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 }
---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,9 @@
+import styles from "../../../public/styles/footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.text}>© 2023 - All rights reserved</p>
+    </footer>
+  );
+}
