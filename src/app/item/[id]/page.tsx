e0aa05879141b1024291c59ca76f48a68a49615a
Alexis-Contente <alexis.contente@gmail.com>
2023-10-17 10:31:04 +0200

    css button add

--- src/app/item/[id]/page.tsx
+++ src/app/item/[id]/page.tsx
@@ -133,28 +133,11 @@ export default function Item({
             </p>
             <button
               type="button"
-              className={styles.button}
+              className={styles.btn}
               onClick={handleAddItem}
             >
-              <span className={styles.button__text}>Add Item</span>
-              <span className={styles.button__icon}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  viewBox="0 0 24 24"
-                  strokeWidth="2"
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  stroke="currentColor"
-                  height="24"
-                  fill="none"
-                  className={styles.svg}
-                >
-                  <line y2="19" y1="5" x2="12" x1="12"></line>
-                  <line y2="12" y1="12" x2="19" x1="5"></line>
-                </svg>
-              </span>
-            </button>{" "}
+              Add
+            </button>
           </div>
         </div>
       )}
