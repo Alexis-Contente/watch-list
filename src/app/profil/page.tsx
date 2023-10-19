42d2e044ba5343fee6f50e9e9d25c837eb65a4fa
Alexis-Contente <alexis.contente@gmail.com>
2023-10-19 10:17:04 +0200

    add loader profil page

--- src/app/profil/page.tsx
+++ src/app/profil/page.tsx
@@ -3,32 +3,42 @@
 import styles from "../../../public/styles/profil.module.css";
 import Footer from "@/components/footer/footer";
 import Header from "@/components/header/header";
+import Loader from "@/components/loader/loader";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Profil() {
   const { data: session } = useSession();
   console.log(session);
+  const [loading, setLoading] = useState(true);
 
-  if (!session) {
-    return <div>Chargement...</div>;
+  useEffect(() => {
+    if (session) {
+      setLoading(false);
+    }
+  }, [session]);
+
+  if (loading) {
+    return <Loader />;
   }
 
   return (
     <div>
       <Header />
+      {}
       <div className={styles.profil__container}>
-        {session.session.user?.image && (
+        {session?.session.user?.image && (
           <Image
-            src={session.session.user?.image}
+            src={session?.session.user?.image}
             alt="Photo de profil de l'utilisateur"
             className={styles.profil__img}
             width={100}
             height={100}
           />
         )}
-        <h1 className={styles.profil__name}>{session.session.user?.name}</h1>
-        <p className={styles.profil__email}>{session.session.user?.email}</p>
+        <h1 className={styles.profil__name}>{session?.session.user?.name}</h1>
+        <p className={styles.profil__email}>{session?.session.user?.email}</p>
       </div>
       <Footer />
     </div>
