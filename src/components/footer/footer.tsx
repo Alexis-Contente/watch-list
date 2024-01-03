bd3b91a0bf26ec221d3b132cb063678e13eb1c11
Alexis-Contente <alexis.contente@gmail.com>
2024-01-03 14:43:02 +0100

    fix copyright

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,7 +3,7 @@ import styles from "../../../public/styles/footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p className={styles.text}>© 2023 - All rights reserved</p>
+      <p className={styles.text}>© 2024 - All rights reserved</p>
     </footer>
   );
 }
